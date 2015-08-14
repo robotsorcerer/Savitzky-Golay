b9ad35c55a122f1325e962204e20d345dedec9c4
lakehanne <patlekano@gmail.com>
2015-08-13 21:58:07 -0500

    started sgolay filter part

--- savgol.cpp
+++ savgol.cpp
@@ -1,14 +1,17 @@
 /*  
 *   MatrixXi vander(const int F, double A[25])
-*     -Computes the vandermonde matrix and flips it from left to right
+*     -Computes the vandermonde matrix and flips it column-wise from left to right
 *
 *   MatrixXd B = MatrixXd sgdiff(int k, double F) 
 *      - designs a Savitzky-Golay (polynomial) FIR smoothing
 *   filter B.  The polynomial order, k, must be less than the frame size of the convolution coefficients,
-*   F, and F must be odd. 
+*   F. F must be odd. 
 *
 *   Author: Olalekan Ogunmolu  
 *           August 12, 2015
+*   
+    Reference: INTRODUCTION TO SIGNAL PROCESSING [Chapter 8; Section 8.3.5]
+                Sophocles J. Orfanidis, Prentice Hall, 2010
 */
 
 // Include Files
@@ -22,13 +25,16 @@
 using namespace Eigen;
 using namespace std;
 
+//Global Variables
+int F = 5;      //Frame Size
+int k = 3;      //Example Polynomial Order
+
 // Function Prototypes
 MatrixXi vander(const int F, double A[25]);
+MatrixXd sgdiff(int k, double Fd);
+void savgolfilt(VectorXf x, int k, int F, MatrixXd DIM);
 
-//Global Variables
-int F = 5;
-
-double A[25];
+double A[25];   //Default vandermonde matrix
 
 class RMatrix
 {
@@ -39,7 +45,7 @@ class RMatrix
 
 MatrixXi vander(const int F, double A[25])
 {
-  VectorXi v = VectorXi::LinSpaced(F,(-(F-1)/2),((F-1)/2)).transpose();
+  VectorXi v = VectorXi::LinSpaced(F,(-(F-1)/2),((F-1)/2)).transpose().eval();
   //cout << "v: " << v << endl;
 
   int j;
@@ -62,9 +68,6 @@ MatrixXi vander(const int F, double A[25])
       A[(j - (k + 1) * F) + 20] *= A[(j - k * F) + 20];
     }
   }
-  /*
-  for (int i = 0; i < 25; ++i)
-  cout << "\nA" << "[" << i << "]: " << A[i] << endl;*/
 
    MatrixXi A_fliplr(F,F);                      //flip the resulting matrix from left to right
    A_fliplr << A[20], A[15], A[10], A[5], A[0], 
@@ -78,11 +81,11 @@ MatrixXi vander(const int F, double A[25])
 
 /*Compute S-Golay Matrix of differentiators*/
 
-void sgdiff(int k, double Fd)
+MatrixXd sgdiff(int k, double Fd)
 {
   //We set the weighting matrix to an identity matrix if no weighting matrix is supplied
   MatrixXd W = MatrixXd::Identity(Fd, Fd);      
-  cout << "\nWeighting Matrix: \n" << W << endl;
+//  cout << "\nWeighting Matrix: \n" << W << endl;
 
   //Compute Projection Matrix B
   MatrixXi s = vander(F, A);   
@@ -90,22 +93,22 @@ void sgdiff(int k, double Fd)
   //Retrieve vandermonde block from projection matrix
   MatrixXi S = s.block(0, 0, s.rows(), (k+1) ) ; 
 
-  cout << "\nVandermonde Matrix: \n" << S << endl;
+//  cout << "\nVandermonde Matrix: \n" << S << endl;
 
   //Compute sqrt(W)*S
   MatrixXd Sd = S.cast<double> ();    //cast S to double
   MatrixXd inter = W * Sd;              //W is assumed to be identity. Change this if you have reasons to.
-  cout << "\nIntermediate matrix: \n" << inter << endl;
+//  cout << "\nIntermediate matrix: \n" << inter << endl;
 
   //Compute the QR Decomposition
   HouseholderQR<MatrixXd> qr(inter);
   qr.compute(inter);
 
-  FullPivLU<MatrixXd>lu_decomp(inter);
+  FullPivLU<MatrixXd>lu_decomp(inter);      //retrieve rank of matrix
   
   int Rank = lu_decomp.rank() ;
 
-  cout <<"\nInter's Rank is: " << Rank << endl;
+//  cout <<"\nInter's Rank is: " << Rank << endl;
 
   //MatrixXd Rnew = RMatrix.R;
 /*
@@ -121,32 +124,40 @@ void sgdiff(int k, double Fd)
     cout << "\n Matrix G: \n" << G << endl;
   }
 
-  else    */      //For rank deficient matrices
-              
+  else    */      
+    //For rank deficient matrices        
     MatrixXd Q = qr.householderQ();
     MatrixXd R = qr.matrixQR().topLeftCorner(Rank, Rank).template triangularView<Upper>();
-    //MatrixXd R = qr.matrixR().topLeftCorner(Rank, Rank).template triangularView<Upper>();  //retrieve the R - Matrix
-    cout << "\n Q is: " << Q << endl;
-    cout <<"\n R with rank deficiency: \n" << R << endl; 
+  //  cout << "\n Q is: " << Q << endl;
+  //  cout <<"\n R with rank deficiency: \n" << R << endl; 
 
     //Compute Matrix of Differentiators
     MatrixXd Rinv = R.inverse();
     MatrixXd RinvT = Rinv.transpose();
-    MatrixXd G = Sd * Rinv * RinvT;
-
+    MatrixXd G = Sd * Rinv * RinvT;           //G is the matrix of differentiation filters
+/*
     cout << "\n R inverse: \n" << Rinv << endl;
     cout << "\n R inv transpose: \n" << RinvT << endl;
     cout << "\n Matrix G: \n" << G << endl;
+    */
  
     MatrixXd SdT = Sd.transpose().eval();
 
-    cout <<"\nInter Transposed: \n" << SdT << endl;
+    //cout <<"\nInter Transposed: \n" << SdT << endl;
 
     //Find the matrix of differentiators
     MatrixXd B = G * SdT * W;
-    cout << "\n Matrix B: \n" << B << endl;
-//  }
 
+    //cout << "\n Matrix B: \n" << B << endl;
+
+    return B;
+}
+
+void savgolfilt(VectorXf x, int k, int F)
+{  
+  Matrix4d DIM = Matrix4d::Zero();        //initialize DIM as a matrix of zeros if it is not supplied
+  cout <<"\nDIM: \n" << DIM <<endl;
+  //Reshape depth values by working along the first non-singleton dimension
 
 }
 
@@ -155,13 +166,17 @@ int main ()
 {
   MatrixXi s = vander(F, A);        //Compute vandermonde matrix
 
-  cout << "\n A_fliplr: \n" << s  << endl;
+  cout << "\n Vandermonde Matrix: \n" << s  << endl;
 
-  double Fd = 5.0;        //sets the frame size for the savgol differentiation coefficients. This must be odd
+  double Fd = (double) F;        //sets the frame size for the savgol differentiation coefficients. This must be odd
 
-  int k = 3;
+  MatrixXd B = sgdiff(k, Fd);
 
-  sgdiff(k, Fd);
+  cout << "\nMatrix of Differentiators: \n" << B << endl;
+
+  VectorXf x = VectorXf::LinSpaced(80, 900, 980);
+
+  savgolfilt(x, k, F);
 
   return 0;
 }
